ef3f2dec7c97fd00ced2ed899e3d4fbdfa9327c2
Nicolas Botello <nbotell2@gmail.com>
2018-06-16 21:28:42 -0700

    add udpate anime, search anime, get user list

--- dist/example/index.js
+++ dist/example/index.js
@@ -5,18 +5,17 @@ const result = dotenv.config()
 function main() {
     const token = process.env.TOKEN;    
     const aniClient = new Anilist(token);
-     const params = {
-        "listType": "ANIME",
-        "status": "CURRENT"
-    };
-    aniClient.getUserListById(params)
-        .then((result) => {
-            result.MediaListCollection.lists[0].entries.forEach(
-              element => {
-                console.log(element.media.title.userPreferred);
-              }
-            );
+    aniClient
+      .getUserList()
+      .then(result => {
+        const media = result.MediaListCollection.lists[0].entries.find((val) =>  val.mediaId === 100240);
+        console.log("This is the element: %O", media);
+        return media.mediaId;
         })
-        .catch((err) => console.log("There was an error %O", err));
+        .then((id) => aniClient.updateAnime({mediaId:id, progress: 2}))
+        .then((result) => {
+            console.log('This is what the result of update is: %O', result);
+        })
+      .catch(err => console.log("There was an error %O", err));
 }
 main();
