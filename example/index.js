32929b22548cb268e107f9802bde66889ed7063f
Nicolas Botello <nbotell2@gmail.com>
2019-05-24 20:07:18 -0700

    update example

--- example/index.js
+++ example/index.js
@@ -3,20 +3,22 @@ const dotenv = require('dotenv'); // eslint-disable-line
 
 dotenv.config();
 function main() {
-  const token = process.env.TOKEN;
+  const token = process.env.QA_TOKEN;
   const aniClient = new Anilist(token);
   aniClient
     .getUserList()
-    .then((result) => {
-      const media = result.MediaListCollection.lists[0].entries.find(val => val.mediaId === 100240);
+    .then(result => {
+      const media = result.MediaListCollection.lists[0].entries.find(
+        val => val.mediaId === 100240
+      );
       console.log('This is the element: %O', media);
       return media.mediaId;
     })
     .then(id => aniClient.updateAnime({ mediaId: id, progress: 2 }))
-    .then((result) => {
+    .then(result => {
       console.log('This is what the result of update is: %O', result);
     })
-    .catch((err) => {
+    .catch(err => {
       console.log('There was an error %O', err);
       process.exit(1);
     });
